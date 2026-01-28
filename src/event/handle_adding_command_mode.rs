196313a62cb9ce80914b74f70c5c0771502d3a9a
ushiboy <ushiboy.dev@gmail.com>
2026-01-28 22:22:38 +0900

    Add command draft

--- src/event/handle_adding_command_mode.rs
+++ src/event/handle_adding_command_mode.rs
@@ -1,4 +1,4 @@
-use crate::app::App;
+use crate::app::{App, add_command};
 use anyhow::Result;
 use crossterm::event::{KeyCode, KeyEvent};
 
@@ -10,6 +10,12 @@ pub async fn handle_adding_command_mode(app: &mut App, key: KeyEvent) -> Result<
         }
         KeyCode::Backspace => {
             app.form_mut().pop_char();
+        }
+        KeyCode::Enter => {
+            let form = app.form();
+            let command_text = form.command_input().to_string();
+            add_command(app, command_text);
+            app.change_normal_mode();
         }
         _ => { /* Ignore other keys */ }
     }
