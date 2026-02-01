4d1aeefd3a7a678bac4cac36ac04020cd9714dcf
ushiboy <ushiboy.dev@gmail.com>
2026-02-01 20:51:11 +0900

    Chore output line

--- src/app/model/output_line.rs
+++ src/app/model/output_line.rs
@@ -1,22 +1,29 @@
 use chrono::{DateTime, Local};
 
+// Represents a single line of output with a timestamp.
 #[derive(Debug, Clone)]
 pub struct OutputLine {
+    // Timestamp when the output line was created.
     timestamp: DateTime<Local>,
+    // The actual content of the output line.
     content: String,
 }
 
 impl OutputLine {
-    pub fn new(content: String) -> Self {
+    // Creates a new OutputLine with the current timestamp and given content.
+    pub fn new(content: impl Into<String>) -> Self {
         OutputLine {
             timestamp: Local::now(),
-            content,
+            content: content.into(),
         }
     }
+
+    // Returns the timestamp of the output line.
     pub fn timestamp(&self) -> &DateTime<Local> {
         &self.timestamp
     }
 
+    // Returns the content of the output line.
     pub fn content(&self) -> &str {
         &self.content
     }
@@ -27,8 +34,11 @@ mod tests {
     use super::*;
     #[test]
     fn test_output_line_new() {
-        let content = "Test output line".to_string();
-        let output_line = OutputLine::new(content.clone());
+        let content = "Test output line";
+
+        let output_line = OutputLine::new(content);
+
         assert_eq!(output_line.content(), content);
+        assert!(output_line.timestamp() <= &Local::now());
     }
 }
