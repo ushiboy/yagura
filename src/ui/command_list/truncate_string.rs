e5990a2798835c1dc4fea560a6abe618067c242c
ushiboy <ushiboy.dev@gmail.com>
2026-02-21 13:05:41 +0900

    Fix support multibyte strings

--- src/ui/command_list/truncate_string.rs
+++ src/ui/command_list/truncate_string.rs
@@ -1,7 +1,19 @@
+use unicode_width::UnicodeWidthChar;
+
 pub fn truncate_string(s: &str, max_len: usize) -> String {
-    if s.len() <= max_len {
-        format!("{:.<width$}", s, width = max_len)
-    } else {
-        format!("{}...", &s[..max_len - 3])
+    let mut width = 0;
+    let mut result = String::new();
+
+    for c in s.chars() {
+        let char_width = c.width().unwrap_or(0);
+
+        if width + char_width > max_len.saturating_sub(3) {
+            result.push_str("...");
+            return format!("{:.<width$}", result, width = max_len);
+        }
+        result.push(c);
+        width += char_width;
     }
+
+    format!("{:.<width$}", result, width = max_len)
 }
