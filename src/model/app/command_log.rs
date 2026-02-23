caf1b64f76e8ed625e1ef4cb84b1b66fa53f0ca9
ushiboy <ushiboy.dev@gmail.com>
2026-02-23 14:27:31 +0900

    Split command_log

---
+++ src/model/app/command_log.rs
@@ -0,0 +1,43 @@
+use super::App;
+
+impl App {
+    pub fn get_command_log_offset(&self) -> Option<usize> {
+        let index = self.ui_state.selected_command_index()?;
+        let command = self.commands.get(index)?;
+        self.ui_state.get_command_log_offset(command.id())
+    }
+
+    pub fn page_down_command_log(&mut self, viewport_height: usize) {
+        if let Some(index) = self.ui_state.selected_command_index()
+            && let Some(command) = self.commands.get(index)
+            && let Some(current_offset) = self.ui_state.get_command_log_offset(command.id())
+        {
+            let id = command.id();
+            let total_lines = command.output_buffer().line_length();
+            let max_offset = total_lines.saturating_sub(viewport_height);
+            let new_offset = (current_offset + viewport_height).min(max_offset);
+
+            if current_offset == new_offset {
+                self.ui_state.remove_command_log_offset(id);
+            } else {
+                self.ui_state.set_command_log_offset(id, new_offset);
+            }
+        };
+    }
+
+    pub fn page_up_command_log(&mut self, viewport_height: usize) {
+        if let Some(index) = self.ui_state.selected_command_index()
+            && let Some(command) = self.commands.get(index)
+        {
+            let id = command.id();
+            let total_lines = command.output_buffer().line_length();
+            let max_offset = total_lines.saturating_sub(viewport_height);
+            let current = self
+                .ui_state
+                .get_command_log_offset(id)
+                .unwrap_or(max_offset);
+            let new_offset = current.saturating_sub(viewport_height);
+            self.ui_state.set_command_log_offset(id, new_offset);
+        }
+    }
+}
