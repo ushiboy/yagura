59575219f2ce43ada936c19c9690241d15518d61
ushiboy <ushiboy.dev@gmail.com>
2026-02-26 22:12:02 +0900

    Add yank help

--- src/ui/output_area_help_bar.rs
+++ src/ui/output_area_help_bar.rs
@@ -7,7 +7,7 @@ use ratatui::{Frame, layout::Rect, text::Line, widgets::Paragraph};
 
 pub fn render(frame: &mut Frame, area: Rect, _app: &App) {
     let help_text = vec![Line::from(Span::styled(
-        "[Ctrl + f/b/j/k] Page Down / Page Up / Line Down / Line Up  [t] Timestamp",
+        "[Ctrl + f/b/j/k] Page Down / Page Up / Line Down / Line Up  [t] Timestamp  [y] Yank",
         Style::default().fg(Color::White),
     ))];
     let help = Paragraph::new(help_text).alignment(Alignment::Center);
